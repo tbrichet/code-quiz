7cb59998bc09efe13d5041adb95c8da8bcb04e99
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-08-22 15:02:08 -0700

    inital setup

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Timer
+
+// Question and Answer Arrays
+
+// Function to display question
+
+// Right & Wrong Answer Conditional Statements
+
+// Score Tracking
+
+// Function to End Quiz
+
+// Event Listener for Buttons
