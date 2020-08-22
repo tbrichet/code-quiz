275d119a15df315523a0fe242d846a78f214cdc3
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-08-22 15:55:43 -0700

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,48 @@
-// Timer
+// Get Elements
+    //Timer
+    var timerEl = document.getElementById("countdown");
+
+    //Start Button
+    var startBtn = document.getElementById("start");
+
+// Timer Countdown Function
+function countdown() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
+            timerEl.textContent = "Time: " + timeLeft;
+            timeLeft--;
+        }
+        else if (timeLeft === 0) {
+            timerEl.textContent = "Time: " + timeLeft;
+            clearInterval(timeInterval);
+            alert ("Time is up! Let's see how you did.")
+            // add end quiz function
+        }
+    }, 1000);
+};
+
 
 // Question and Answer Arrays
 
-// Function to display question
+// Display initial screen
 
-// Right & Wrong Answer Conditional Statements
+// Function to start quiz
 
-// Score Tracking
+startBtn.onclick = countdown;
+
+// Right & Wrong Answer Function with Conditional Statements
+
+// High Score Tracking
+
+// High Score Display
 
 // Function to End Quiz
 
-// Event Listener for Buttons
+// Return to Quiz
+
+// Clear High Scores
+
+// Event Listeners for Buttons
+
