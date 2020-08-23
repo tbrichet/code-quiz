6b31301b5cc8f9db9a0763ef13212ff65bb31955
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-08-22 22:23:16 -0700

    question array added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@
     //Start Button
     var startBtn = document.getElementById("start");
 
+    //Quiz and Questions
+    var quiz = document.getElementById("quiz");
+    var question = document.getElementById("question");
+
+    //Answer Choices
+
+    var choiceA = document.getElementById("A");
+    var choiceB = document.getElementById("B");
+    var choiceC = document.getElementById("C");
+    var choiceD = document.getElementById("D");
+
 // Timer Countdown Function
 function countdown() {
     var timeLeft = 60;
@@ -24,7 +35,50 @@ function countdown() {
 };
 
 
-// Question and Answer Arrays
+// Questions Array
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choiceA: "strings",
+        choiceB: "booleans",
+        choiceC: "alerts",
+        choiceD: "numbers",
+        correct: "C"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        choiceA: "quotes",
+        choiceB: "curly brackets",
+        choiceC: "parentheses",
+        choiceD: "square brackets",
+        correct: "C"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______",
+        choiceA: "numbers and strings",
+        choiceB: "other arrays",
+        choiceC: "booleans",
+        choiceD: "all of the above",
+        correct: "D"
+    },
+    {
+        question: "String values must be enclosed with _____ when being assigned to variables.",
+        choiceA: "commas",
+        choiceB: "curly brackets",
+        choiceC: "quotes",
+        choiceD: "parentheses",
+        correct: "C"
+    },
+    {
+        question: "A very useful tool during development and debugging for printing content to the debugger is:",
+        choiceA: "JavaScript",
+        choiceB: "terminal/bash",
+        choiceC: "for loops",
+        choiceD: "console.log",
+        correct: "D"
+    }
+]
 
 // Start Timer and Quiz
 
